63217694854ad3c41403c06f4174b11ea75fed24
Marvelousy20 <afosmarvel@gmail.com>
2023-02-03 01:54:46 +0100

    Initial Commit

---
+++ Notification/NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationApp.swift
+//  Notification
+//
+//  Created by Marvelous Afolabi on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
